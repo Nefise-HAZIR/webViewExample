9f67139c3614339b48744d00a77d7927b6af763e
nefise <2019555034@ogr.cu.edu.tr>
2021-10-01 14:13:00 +0300

    .

---
+++ app/src/main/java/com/example/webviewexample/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.webviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView=findViewById(R.id.web);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.google.com");
+
+
+    }
+
+    public  void onBackPressed(){
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+
+
+        
+    }
+}
